0db890d063a15961b7371abc9f3b768d50044c71
Psycho Asto <kuvaldinms@gmail.com>
2024-07-24 21:55:11 +0300

    little fix

--- main.py
+++ main.py
@@ -1,22 +1,12 @@
 import asyncio
 import subprocess
 
+import uvicorn
 from fastapi import FastAPI
 
 from src.api import horoscope_router
 from src.db.database import create_tables, drop_tables
 from src.broker import broker, daily_task, scheduler
 
-
 app = FastAPI(title="Horo Scopes")
 app.include_router(horoscope_router)
-
-
-async def main():
-    # await drop_tables()
-    # await create_tables()
-    print("Application startup successfully!")
-
-
-if __name__ == '__main__':
-    asyncio.run(main())
